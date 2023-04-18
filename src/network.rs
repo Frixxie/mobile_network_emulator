6e4d7f7db4d0a86eca2ea4adfd268770f0f8bb2f
Fredrik Hagen Fasteraune <frha@dips.no>
2023-04-18 14:23:31 +0200

    more dev on network

--- src/network.rs
+++ src/network.rs
@@ -1,8 +1,6 @@
 use std::{error::Error, fmt::Display, time::Duration};
 
-use url::Url;
-
-use crate::edge_data_center::EdgeDataCenter;
+use crate::{application::Application, edge_data_center::EdgeDataCenter};
 
 #[derive(Debug)]
 struct NetworkError {
@@ -34,7 +32,88 @@ impl Network {
         Network { edge_data_centers }
     }
 
-    pub fn use_application(&mut self, url: &Url) -> Result<(), NetworkError> {
-        todo!();
+    pub fn use_application(&mut self, application: &Application) -> Result<(), NetworkError> {
+        match self
+            .edge_data_centers
+            .iter_mut()
+            .find(|(edge_data_center, _delay)| {
+                edge_data_center.contains_application(application.url())
+            }) {
+            Some((edge_data_center, _duration)) => {
+                //We know that the edge data center has the application.
+                let _usage = edge_data_center.use_application(application).unwrap();
+                Ok(())
+            }
+            None => Err(NetworkError::new(&format!(
+                "Application on url {} does not exist",
+                application.url()
+            ))),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter::repeat;
+
+    use geo::Point;
+    use url::Url;
+
+    #[test]
+    fn create() {
+        let edge_data_centers = repeat((
+            "Fredrik's edge data center",
+            Point::new(0.0, 0.0),
+            Duration::from_secs(1),
+        ))
+        .take(32)
+        .map(|(name, position, delay)| (EdgeDataCenter::new(name, position), delay))
+        .collect();
+
+        let network = Network::new(edge_data_centers);
+
+        assert_eq!(network.edge_data_centers.iter().count(), 32);
+    }
+
+    #[test]
+    fn use_application() {
+        let mut edge_data_centers: Vec<(EdgeDataCenter, Duration)> = repeat((
+            "Fredrik's edge data center",
+            Point::new(0.0, 0.0),
+            Duration::from_secs(1),
+        ))
+        .take(2)
+        .map(|(name, position, delay)| (EdgeDataCenter::new(name, position), delay))
+        .collect();
+        let application = Application::new(Url::parse("http://fasteraune.com").unwrap(), 0);
+        edge_data_centers[0]
+            .0
+            .add_application(&application)
+            .unwrap();
+        let mut network = Network::new(edge_data_centers);
+
+        let result = network.use_application(&application);
+
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn use_application_not_present_should_fail() {
+        let edge_data_centers: Vec<(EdgeDataCenter, Duration)> = repeat((
+            "Fredrik's edge data center",
+            Point::new(0.0, 0.0),
+            Duration::from_secs(1),
+        ))
+        .take(1)
+        .map(|(name, position, delay)| (EdgeDataCenter::new(name, position), delay))
+        .collect();
+        let application = Application::new(Url::parse("http://fasteraune.com").unwrap(), 0);
+
+        let mut network = Network::new(edge_data_centers);
+
+        let result = network.use_application(&application);
+
+        assert_eq!(result.is_ok(), false);
     }
 }
