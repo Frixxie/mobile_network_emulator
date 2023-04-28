fedb5edfe94178fc3ddb44b890a217a2ed653ed6
Fredrik Hagen Fasteraune <frha@dips.no>
2023-04-28 10:30:42 +0200

    Adding location reporting, closes #2

--- src/mobile_network_core_event.rs
+++ src/mobile_network_core_event.rs
@@ -1,8 +1,80 @@
 use std::net::{Ipv4Addr, Ipv6Addr};
 
+use geo::{Point, Polygon};
+
 pub enum EventType {
-    MonitoringEvent,
     PdnConnectionEvent(PdnConnectionInformation),
+    LocationReporting(LocationInfo),
+}
+
+pub struct LocationInfo {
+    age_of_location_info: u32,
+    cell_id: String,
+    e_node_b_id: String,
+    routing_area_id: String,
+    tracking_area_id: String,
+    plmn_id: String,
+    twan_id: String,
+    geographic_area: GeographicArea,
+    civic_address: CivicAddress,
+    position_method: Vec<PositioningMethod>,
+    qos_fulfill_ind: AccuracyFulfillmentIndicator,
+    ue_velocity: VelocityEstimate,
+    ldr_type: LdrType,
+    achieved_qos: MinorLocationQoS,
+}
+
+pub enum GeographicArea {
+    Point(Point),
+    PointUncertainCircle,
+    PointUncertaintyEllipse,
+    Polygon(Polygon),
+    PointAltitude,
+    PointAlititudeUncertainity,
+    EllipsoidArc,
+}
+
+//We do not care about this struct it should just be there i guess
+pub struct CivicAddress;
+
+pub enum PositioningMethod {
+    Cellid,
+    Ecid,
+    Otdoa,
+    BarometricPressure,
+    Wlan,
+    Bluetooth,
+    Mbs,
+    MotionSensor,
+    DlTdoa,
+    DlAod,
+    MultiRtt,
+    NrEcid,
+    UlTdoa,
+    UlAoa,
+    NetworkSpecific,
+}
+
+pub enum AccuracyFulfillmentIndicator {
+    RequestedAccuracyFulfilled,
+    RequestedAccuracyNotFulfilled,
+}
+
+//Based on spec this is way more complicated
+pub type VelocityEstimate = f64;
+
+pub enum LdrType {
+    UeAvailable,
+    Periodic,
+    EnteringIntoArea,
+    LeavingFromArea,
+    BeingInsideArea,
+    Motion,
+}
+
+pub struct MinorLocationQoS {
+    h_accuracy: f64,
+    v_accuracy: f64,
 }
 
 pub enum PdnType {
