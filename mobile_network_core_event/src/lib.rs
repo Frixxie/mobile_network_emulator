4e42e9b630bd15b7151c1877480cd9b3c53ccedb
Fredrik Hagen Fasteraune <frha@dips.no>
2023-05-12 13:00:15 +0200

    should fix build error

--- mobile_network_core_event/src/lib.rs
+++ mobile_network_core_event/src/lib.rs
@@ -1,6 +1,6 @@
 mod mobile_network_core_event;
 
-pub use mobile_network_core_event::{
+pub use crate::mobile_network_core_event::{
     AccuracyFulfillmentIndicator, CivicAddress, Event, EventKind, EventKind::LocationReporting,
     EventKind::PdnConnectionEvent, GeographicArea, InterfaceIndication, LdrType, LocationInfo,
     MinorLocationQoS, MobileNetworkCoreEvent, PdnConnectionInformation, PdnConnectionStatus,
